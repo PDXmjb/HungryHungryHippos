6f9ab10544b46b6fad6b1afa0258732792353794
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-02-28 17:07:58 -0800

    Figures out the number of "bites" required to complete a game of Hungry Hungry Hippos, if the marbles are in a 2d array and it takes one bite to get all adjacent marbles.

---
+++ HungryHungryHippos/HungryHipposTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars
+{
+    [TestFixture]
+    class KataTestClass
+    {
+        [TestCase]
+        public void BasicTest1()
+        {
+            int[,] board = { { 1, 1, 0, 0, 0 },
+                             { 1, 1, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 1, 1 },
+                             { 0, 0, 0, 1, 1 } };
+            Game game = new Game(board);
+            Assert.AreEqual(game.play(), 2, "Should return '2'");
+        }
+
+        [TestCase]
+        public void BasicTest2()
+        {
+            int[,] board = { { 1, 0, 1, 0, 1 },
+                             { 1, 0, 1, 0, 1 },
+                             { 1, 1, 1, 0, 0 },
+                             { 0, 0, 0, 1, 1 },
+                             { 0, 0, 0, 1, 1 } };
+            Game game = new Game(board);
+            Assert.AreEqual(game.play(), 3, "Should return '3'");
+        }
+
+        [TestCase]
+        public void BasicTest3()
+        {
+            int[,] board = { { 1, 0, 1, 0, 1 },
+                             { 0, 1, 0, 1, 0 },
+                             { 1, 0, 1, 0, 1 },
+                             { 0, 1, 0, 1, 0 },
+                             { 1, 0, 1, 0, 1 } };
+            Game game = new Game(board);
+            Assert.AreEqual(game.play(), 13, "Should return '13'");
+        }
+
+        [TestCase]
+        public void BasicTest4()
+        {
+            int[,] board = { { 1, 0, 0, 0, 0 },
+                             { 0, 0, 1, 1, 0 },
+                             { 1, 0, 1, 0, 1 },
+                             { 1, 1, 1, 1, 0 },
+                             { 1, 1, 1, 0, 1 } };
+            Game game = new Game(board);
+            Assert.AreEqual(game.play(), 4, "Should return '4'");
+        }
+    }
+}
--- HungryHungryHippos/Program.cs
+++ HungryHungryHippos/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HungryHungryHippos
+namespace CodeWars
 {
-    class Program
+    public class Game
     {
-        static void Main(string[] args)
+        private int[,] Board { get; set; }
+
+        public Game(int[,] board)
         {
+            Board = board;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[,] board = { { 1, 1, 0, 0, 0 },
+                             { 1, 1, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 1, 1 },
+                             { 0, 0, 0, 1, 1 } };
+            Game game = new Game(board);
+            Console.WriteLine(game.play());
+        }
+
+        public int play()
+        {
+            var leaps = 0;
+
+            for (int y = 0; y < Board.GetLength(1); y++)
+            {
+                for (int x = 0; x < Board.GetLength(0); x++)
+                {
+                    if (Board[y, x] == 1)
+                    {
+                        leaps++;
+                        RemoveBlock(y, x);
+                    }
+                }
+            }
+            return leaps;
+        }
+
+        public void RemoveBlock(int y, int x)
+        {
+            // Flip to zero
+            Board[y, x] = 0;
+            var length = Board.GetLength(0);
+
+            // Check left
+            if (x > 0 && Board[y, x - 1] == 1)
+            {
+                RemoveBlock(y, x - 1);
+            }
+            // Check right
+            if (x < length - 1 && Board[y, x + 1] == 1)
+            {
+                RemoveBlock(y, x + 1);
+            }
+            // Check up
+            if (y > 0 && Board[y - 1, x] == 1)
+            {
+                RemoveBlock(y - 1, x);
+            }
+            // Check down
+            if (y < length - 1 && Board[y + 1, x] == 1)
+            {
+                RemoveBlock(y + 1, x);
+            }
         }
     }
 }
